890583fc709de2cc9c4fb9fc5ba28ac23e0e22ed
aswazone <aswamanu143@gmail.com>
2024-08-30 14:23:08 +0530

    pizza-shop

--- Bulit-in-module/event.js
+++ Bulit-in-module/event.js
@@ -1,24 +1,32 @@
-// EventEmitter is a class requires from the node:events
-const EventEmitter = require("node:events");
-// emiter enna oru object Class n vendi indakki
-const emitter = new EventEmitter();
+const Pizzashop = require("./pizza-shop");
 
-//there will be a listener on to register the method
-emitter.on("order-pizza",()=> {//event name (order-pizza) same aayirikkyanmh
-    console.log("Order is received!!");
-})
+const pizzashop = new Pizzashop();
 
-// track the event or emit the event
-//there will be a emit to track the event
+pizzashop.order();
+pizzashop.order();
+pizzashop.displayOrderNumber();
 
-emitter.emit("order-pizza");//ingane ezhudhanamh...emit n sheshamhh on
-//there will be a listener on
+// // EventEmitter is a class requires from the node:events
+// const EventEmitter = require("node:events");
+// // emiter enna oru object Class n vendi indakki
+// const emitter = new EventEmitter();
 
-// with para meter
-emitter.on("order-pizza",(size,topping)=> {
-    console.log(`Order is received!! ${size} with ${topping}`);
-})
+// //there will be a listener on to register the method
+// emitter.on("order-pizza",()=> {//event name (order-pizza) same aayirikkyanmh
+//     console.log("Order is received!!");
+// })
 
-// track the event or emit the event
-//there will be a emit to track the event
-emitter.emit("order-pizza","large","cheesy");//ingane ezhudhanamh...emit n sheshamhh on
+// // track the event or emit the event
+// //there will be a emit to track the event
+
+// emitter.emit("order-pizza");//ingane ezhudhanamh...emit n sheshamhh on
+// //there will be a listener on
+
+// // with para meter
+// emitter.on("order-pizza",(size,topping)=> {
+//     console.log(`Order is received!! ${size} with ${topping}`);
+// })
+
+// // track the event or emit the event
+// //there will be a emit to track the event
+// emitter.emit("order-pizza","large","cheesy");//ingane ezhudhanamh...emit n sheshamhh on
